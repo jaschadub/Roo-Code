e1097cb27df8ad55a5445ebb94ca85444c8bcdba
Jascha <jaschadub@gmail.com>
2025-06-20 23:23:45 -0700

    custom modes

--- webview-ui/src/components/welcome/RooHero.tsx
+++ webview-ui/src/components/welcome/RooHero.tsx
@@ -11,15 +11,15 @@ const RooHero = () => {
 			<div
 				style={{
 					backgroundColor: "var(--vscode-foreground)",
-					WebkitMaskImage: `url('${imagesBaseUri}/roo-logo.svg')`,
+					WebkitMaskImage: `url('${imagesBaseUri}/../icons/icon.svg')`,
 					WebkitMaskRepeat: "no-repeat",
 					WebkitMaskSize: "contain",
-					maskImage: `url('${imagesBaseUri}/roo-logo.svg')`,
+					maskImage: `url('${imagesBaseUri}/../icons/icon.svg')`,
 					maskRepeat: "no-repeat",
 					maskSize: "contain",
 				}}
 				className="mx-auto">
-				<img src={imagesBaseUri + "/roo-logo.svg"} alt="Roo logo" className="h-8 opacity-0" />
+				<img src={imagesBaseUri + "/../icons/icon.svg"} alt="Roo logo" className="h-8 opacity-0" />
 			</div>
 		</div>
 	)
